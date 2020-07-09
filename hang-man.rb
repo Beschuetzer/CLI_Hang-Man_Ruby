a302de7ab2523d00e65563f06d5e36d76b360d76
Adam Major <adam.j.major@gmail.com>
2020-07-08 19:53:55 -0700

    Can play game but no hangman draw yet

--- hang-man.rb
+++ hang-man.rb
@@ -8,7 +8,8 @@ class HangManGame
   @@valid_words = []
 
   def initialize(max_chances, max_word_size)
-    @chances_left = max_chances
+    @max_chances = max_chances
+    @chances_left = @max_chances
     @max_word_size = max_word_size    
     setup_variables()
     populate_valid_words()
@@ -17,12 +18,28 @@ class HangManGame
   def start()
     print_instructions()
     get_secret_word    
+    draw_grid
     while @chances_left > 0 do
-      @grid = []
-      puts "While loop"
-      draw_grid
       pick_letter
+      draw_grid
+      puts "@secret_word_display_string: #{@secret_word_display_string}"
+      if !@secret_word_display_string.include?('_')
+        puts "Great job!  '#{@secret_word}'' is the word.  Play again? " 
+        break
+      end
+
     end
+    puts "You are out of guesses.  Play again? " if @chances_left <= 0
+    ans = gets.chomp
+    while !ans.match(/^\s*[yYnN]([eE][sS]|[oO])*\s*$/) do                                  
+      print "Play again?  Available options are 'y' and 'n': "
+      ans = gets.chomp.downcase
+    end 
+    if ans.match(/y/)
+      setup_variables
+      start
+    end
+
     #start playing
   end
 
@@ -33,7 +50,7 @@ class HangManGame
       @letters_available << char
     }
     @letters_guessed_correctly = []
-    @chances_left_left = @chances_left
+    @chances_left = @max_chances
     @grid_section_word = []
     @grid_section_letters = []
     @grid_section_hangman = []
@@ -71,13 +88,16 @@ class HangManGame
     #section 1: chances_left and secret word
     add_empty_line_to_grid_section(@grid_section_word)
     @grid_section_word << "SECRET WORD".center(@@section_width-1)
+    @secret_word_display_string = "_ " * @secret_word.length
     if @letters_guessed_correctly.length != 0
-      #need to dynamically create this based on @letters_guessed_correctly
-      puts 'Not yet implemented'
-    else
-      substr = "_ " * @secret_word.length
-      @grid_section_word << (substr).center(@@section_width)
+      chars_in_secret_word = @secret_word.split('')
+      #puts "secret word split: #{chars_in_secret_word} and @letters_guessed_correctly #{@letters_guessed_correctly}"
+      chars_in_secret_word.each_index {|index|
+        #puts "@secret_word_display_string[index*2+1]: #{@secret_word_display_string}[index*2+1] and chars_in_secret_word[index]: #{chars_in_secret_word[index]}"
+        @secret_word_display_string[index*2] = chars_in_secret_word[index] if @letters_guessed_correctly.include?(chars_in_secret_word[index].downcase)
+      }
     end
+    @grid_section_word << (@secret_word_display_string).center(@@section_width)
     add_empty_line_to_grid_section(@grid_section_word)
     @grid_section_word << "Guesses: #{@chances_left}".center(@@section_width)
   end
@@ -104,11 +124,15 @@ class HangManGame
 
   def get_grid_section_hangman()
 
-
   end
 
   def draw_grid
     #gets all grid sections, combines, and displays
+    @grid_section_word = []
+    @grid_section_letters = []
+    @grid_section_hangman = []
+    @grid = []
+
     get_grid_section_word
     get_grid_section_letters
     get_grid_section_hangman
@@ -140,9 +164,11 @@ class HangManGame
       next_letter = gets.chomp.to_s
     end while !next_letter.match(/[a-zA-Z]/) || !@letters_available.include?(next_letter)
     #do stuff depending on whether letter is in secret word
+    @letters_available -= [next_letter]
     if @secret_word.include?(next_letter)
-      @letters_available -= [next_letter]
+      @letters_guessed_correctly += [next_letter]
       indexes_of_letters_to_show = find_indexes_of_letter(next_letter)
+      #puts "next_letter: #{next_letter} and @letters_available: #{@letters_available}, and @letters_guessed_correctly: #{@letters_guessed_correctly}"
     else
       @chances_left -= 1
     end
